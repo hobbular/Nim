5f3c5e9180e2481f211f2891b64be165be1e35b9
Hobbes <legault@wisc.edu>
2014-07-10 10:28:45 -0500

    Merge branch 'SampleBranch'
    Conflicts:
    	nim.py

--- nim.py
+++ nim.py
@@ -1,105 +1,122 @@
 import random
 
 #############################################
+# Game board class
+#############################################
+class GameBoard:
+    board = [['|','|','|','|','|'],
+             ['|','|','|','|'],
+             ['|','|','|'],
+             ['|','|']]
+    sticks = [5,4,3,2]
+    human_move = True    # this is how we'll track whose turn it is
+    
+    #############################################
+    # Displays the current state of the board
+    #############################################
+    def display(self):
+        print
+        space_count = 0
+        for line in self.board:
+            print str(space_count+1)+": "+(space_count*' '),
+            for item in line:
+                print item,
+            print (space_count*' ')+" ("+str(self.sticks[space_count])+")"
+            space_count += 1
+        print
+
+    #############################################
+    # Changes the board based on the chosen move.
+    # If the move was illegal, returns False.
+    #############################################
+    def make_move(self,pile,count):
+        if not '|' in self.board[pile] or count == 0:
+            # tried to take sticks from an empty pile
+            return False
+        # remove UP TO count sticks from pile
+        removed = 0
+        index = 0
+        while removed < count and '|' in self.board[pile]:
+            # find a stick to remove
+            while self.board[pile][index] != '|':
+                index += 1
+            # remove it
+            self.board[pile][index] = ' '
+            removed += 1
+        self.sticks[pile] -= removed
+        return True
+
+    #############################################
+    # Count number of sticks in a given row
+    #############################################
+    def count_sticks(line):
+        return sum(1 for x in line if x == '|')
+
+    #############################################
+    # Count number of sticks left on the board
+    #############################################
+    def count_board(self):
+        return sum(x for x in self.sticks)
+
+    #############################################
+    # Determines whether the board is empty
+    #############################################
+    def is_empty(self):
+        return '|' in self.board[0] or '|' in self.board[1] or '|' in self.board[2] or '|' in self.board[3]
+
+#############################################
 # My variables for my game
 #############################################
-board = [['|','|','|','|','|'],
-         ['|','|','|','|'],
-         ['|','|','|'],
-         ['|','|']]
-
-human_move = True    # this is how we'll track whose turn it is
-
-#############################################
-# Displays the current state of the board
-#############################################
-def display_board(board):
-    print
-    space_count = 0
-    for line in board:
-        print str(space_count+1)+": "+(space_count*' '),
-        for item in line:
-            print item,
-        print (space_count*' ')+" ("+str(count_sticks(line))+")"
-        space_count += 1
-    print
-
-#############################################
-# Changes the board based on the chosen move.
-# If the move was illegal, returns False.
-#############################################
-def make_move(board,pile,count):
-    if not '|' in board[pile] or count == 0:
-        # tried to take sticks from an empty pile
-        return False
-    # remove UP TO count sticks from pile
-    removed = 0
-    index = 0
-    while removed < count and '|' in board[pile]:
-        # find a stick to remove
-        while board[pile][index] != '|':
-            index += 1
-        # remove it
-        board[pile][index] = ' '
-        removed += 1
-    return True
-
-#############################################
-# Count number of sticks in a given row
-#############################################
-def count_sticks(line):
-    return sum(1 for x in line if x == '|')
-
-#############################################
-# Count number of sticks left on the board
-#############################################
-def count_board(board):
-    return sum(count_sticks(board[x]) for x in range(len(board)))
+board = GameBoard()
+num_piles = len(board.board)
 
 #############################################
 # PLAY THE GAME!!
 #############################################
-while '|' in board[0] or '|' in board[1] or '|' in board[2] or '|' in board[3]:
-    display_board(board)
+while board.count_board() > 0:
+    board.display()
 
     # if it's the human's turn:
-    while human_move:
-        pile_num = int(raw_input("pick a pile 1-4: "))
-        while pile_num < 1 or pile_num > 4:
+    while board.human_move:
+        pile_num = int(raw_input("pick a pile 1-"+str(num_piles)+": "))
+        while pile_num < 1 or pile_num > num_piles:
             pile_num = int(raw_input("that's not one of the options: "))
+        pile_num -= 1
+        if board.sticks[pile_num] == 0:
+            print "You can't remove sticks from an empty pile!"
+            continue
         how_many = int(raw_input("how many sticks: "))
         while how_many < 1:
             how_many = int(raw_input("nice try. gotta pick up at least one: "))
-        pile_num -= 1
-        success = make_move(board,pile_num,how_many)
+        success = board.make_move(pile_num,how_many)
         if success:
             break
-        print "You can't remove sticks from an empty pile!"
     
     # if it's the computer's turn:
-    while not human_move:
+    while not board.human_move:
         pile_num = random.randint(0,3)
-        while count_sticks(board[pile_num]) == 0:
+        while board.sticks[pile_num] == 0:
             pile_num = random.randint(0,3)
-        if count_board(board) == count_sticks(board[pile_num])+1:
+        if board.count_board() == board.sticks[pile_num]+1:
             # if it's about to win, have it take everything in that pile
-            how_many = count_sticks(board[pile_num])
+            how_many = board.sticks[pile_num]+1
         else:
-            how_many = random.randint(1,count_sticks(board[pile_num]))
-        success = make_move(board,pile_num,how_many)
+            how_many = random.randint(1,board.sticks[pile_num])
+        success = board.make_move(pile_num,how_many)
         if success:
             print "Computer removed",how_many,"sticks from pile",pile_num+1
             break
 
     # switch whose turn it is
-    human_move = not human_move
+    board.human_move = not board.human_move
 
 #############################################
 # WHO WON???
 #############################################
-if human_move:
+if board.human_move:
     # the computer had the last move and picked up the last stick
     print "You win!"
+    print "I AM DELETING SYSTEM 32"
 else:
     # you had to pick up the last stick
-    print "You lose!"
+    print "You lose! YOU SUCK"
